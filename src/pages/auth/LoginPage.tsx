a85631d7ad4c3f67ca800a6b031a8de194ce5524
Javi <130647819+JavieraAlmendrasVilla@users.noreply.github.com>
2025-06-01 17:42:31 +0200

    Updated LoginPage.tsx

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import { calendar-heart as Tooth } from 'lucide-react';
+import { Calendar-heart as Tooth } from 'lucide-react';
 import { useLanguage } from '../../lib/i18n/LanguageContext';
 
 const LoginPage = () => {
