5b6a71ad855a728e94a09a89d219732a38edebb3
Javi <130647819+JavieraAlmendrasVilla@users.noreply.github.com>
2025-06-01 19:30:04 +0200

    Updated DentalChartPage.tsx

--- src/pages/dental-chart/DentalChartPage.tsx
+++ src/pages/dental-chart/DentalChartPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Calendar, FilePlus, History, Printer, Save, Clock, X, Plus, Trash2, Bluetooth as Tooth } from 'lucide-react';
+import { Calendar, FilePlus, History, Printer, Save, Clock, X, Plus, Trash2, CalendarHeart as Tooth } from 'lucide-react';
 import DentalChart from './components/DentalChart';
 
 interface Treatment {
